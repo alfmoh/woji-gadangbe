247e88dd526935f9f629dbef4ee7d168b054bcc8
alf <alfmohenu@outlook.com>
2017-10-31 16:48:17 +0300

    Firebase added. Top padding added. Routing added. Heading added.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
+import { RouterModule } from '@angular/router';
+import { environment } from '../environments/environment';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AngularFireModule } from "angularfire2";
+import { AngularFireDatabaseModule } from "angularfire2/database";
+import { AngularFireAuthModule } from "angularfire2/auth";
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './core/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {path: "", component: HomeComponent}
+    ]),
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
